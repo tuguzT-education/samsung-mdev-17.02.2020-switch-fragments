9bc7ac54f9de8a794bf1a1f5308225f699d7cf2e
tuguzT <56771526+tuguzT@users.noreply.github.com>
2020-02-22 23:34:08 +0300

    По нажатию одной из кнопок появляется соответствующий фрагмент

---
+++ app/src/main/java/com/example/a17022020/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.a17022020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn1;
+    Button btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button) {
+            loadFragment(new Fragment1());
+        }
+        else {
+            loadFragment(new Fragment2());
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.replace, fragment);
+        fragmentTransaction.commit();
+    }
+}
